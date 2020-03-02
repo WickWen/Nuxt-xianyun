1fc4b802cf82957e837dd6ea535fbca4683d6f26
WickWen <mrwickweb@163.com>
2020-03-03 03:24:46 +0800

    新增首页轮播图布局

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,52 @@
 <template>
-  <div>
-    首页
+  <div class="container">
+    <!-- 幻灯片 -->
+    <el-carousel :interval="5000" arrow="always">
+      <el-carousel-item v-for="(item, index) in banners" :key="index">
+        <div
+          class="banner-image"
+          :style="`
+                background:url(${item.url}) center center no-repeat;
+                background-size:contain contain;
+                `"
+        ></div>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      banners: [
+        {
+          url:
+          "http://attach.bbs.miui.com/forum/201401/11/145825zn1sxa8anrg11gt1.jpg"
+        },
+        {
+          url:
+          "http://attach.bbs.miui.com/forum/201311/17/174124tp3sa6vvckc25oc8.jpg"
+        }
+      ]
+    };
+  }
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.container {
+  min-width: 1000px;
+  margin: 0 auto;
+  position: relative;
 
+  /deep/ .el-carousel__container {
+    height: 700px;
+  }
+
+  .banner-image {
+    width: 100%;
+    height: 100%;
+  }
+}
 </style>

db05e9a8c85bc8597c2aeaa226b232d697c3bab8
WickWen <mrwickweb@163.com>
2020-03-03 21:44:22 +0800

    tab栏 高亮切换

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <!-- 幻灯片 -->
     <el-carousel :interval="5000" arrow="always">
       <el-carousel-item v-for="(item, index) in banners" :key="index">
+        <!-- 使用v-for遍历已渲染的数组时,默认用就地复用策略;数组数据修改的时候,他会根据key值去判断某个值是否修改,如果修改,则重新渲染这一项,否则复用之前的元素; -->
         <div
           class="banner-image"
           :style="`
@@ -12,6 +13,35 @@
         ></div>
       </el-carousel-item>
     </el-carousel>
+
+    <!-- 搜索框 -->
+    <div class="banner-content">
+      <div class="search-bar">
+        <!-- tab栏 -->
+        <el-row type="flex" class="search-tab">
+          <span
+            v-for="(item, index) in options"
+            :key="index"
+            :class="{active: index === currentOption}"
+            @click="handleOption(index)"
+          >
+            <!-- active 需要添加的class name (key) -->
+            <!-- value 判断是否为truthy（真值）如果为真 则添加  -->
+            <!-- 3.动态给当前的激活的选项 添加class -->
+            <!-- 2. 添加点击事件 改变 currentOption -->
+            <i>{{item.name}}</i>
+          </span>
+        </el-row>
+
+        <!-- 输入框 -->
+        <el-row type="flex" align="middle" class="search-input">
+          <input
+
+          />
+          <i class="el-icon-search"></i>
+        </el-row>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,21 +49,44 @@
 export default {
   data() {
     return {
-      banners: []
+      banners: [],
+      options: [
+        // 搜索框tab选项
+        {
+          name: "攻略",
+          placeholder: "搜索城市",
+          pageUrl: "/post?city="
+        },
+        {
+          name: "酒店",
+          placeholder: "请输入城市搜索酒店",
+          pageUrl: "/hotel?city="
+        },
+        {
+          name: "机票",
+          placeholder: "请输入出发地",
+          pageUrl: "/air"
+        }
+      ],
+      currentOption: 0 /* 1.当前激活选项 */
     };
   },
-  mounted(){
+  mounted() {
     this.getBanners();
   },
-  methods:{
-    async getBanners(){
+  methods: {
+    async getBanners() {
       try {
-        const res = await this.$axios({url:'/scenics/banners'})
-        const {data} = res.data
-        this.banners =data;
+        const res = await this.$axios({ url: "/scenics/banners" });
+        const { data } = res.data;
+        this.banners = data;
       } catch (error) {
         console.log(error);
       }
+    },
+    handleOption(index) {
+      this.currentOption = index;
+      // 点击获取到的 index 赋值到data
     }
   }
 };
@@ -52,6 +105,95 @@ export default {
   .banner-image {
     width: 100%;
     height: 100%;
+  }
+
+  .banner-content {
+    z-index: 9;
+    width: 1000px;
+    position: absolute;
+    left: 50%;
+    top: 45%;
+    margin-left: -500px;
+    border-top: 1px transparent solid;
+
+    .search-bar {
+      width: 552px;
+      margin: 0 auto;
+    }
+
+    .search-tab {
+      .active {
+        i {
+          color: #333;
+        }
+        &::after {
+          background: #eee;
+        }
+      }
+
+      span {
+        width: 82px;
+        height: 36px;
+        display: block;
+        position: relative;
+        margin-right: 8px;
+        cursor: pointer;
+
+        i {
+          position: absolute;
+          z-index: 2;
+          display: block;
+          width: 100%;
+          height: 100%;
+          line-height: 30px;
+          text-align: center;
+          color: #fff;
+        }
+
+        &:after {
+          position: absolute;
+          left: 0;
+          top: 0;
+          display: block;
+          content: "";
+          width: 100%;
+          height: 100%;
+          border: 1px rgba(255, 255, 255, 0.2) solid;
+          border-bottom: none;
+          transform: scale(1.1, 1.3) perspective(0.7em) rotateX(2.2deg);
+          transform-origin: bottom left;
+          background: rgba(0, 0, 0, 0.5);
+          border-radius: 1px 2px 0 0;
+          box-sizing: border-box;
+        }
+      }
+    }
+
+    .search-input {
+      width: 550px;
+      height: 46px;
+      background: #fff;
+      border-radius: 0 4px 4px 4px;
+      border: 1px rgba(255, 255, 255, 0.2) solid;
+      border-top: none;
+      box-sizing: unset;
+
+      input {
+        flex: 1;
+        height: 20px;
+        padding: 13px 15px;
+        outline: none;
+        border: 0;
+        font-size: 16px;
+      }
+
+      .el-icon-search {
+        cursor: pointer;
+        font-size: 22px;
+        padding: 0 10px;
+        font-weight: bold;
+      }
+    }
   }
 }
 </style>

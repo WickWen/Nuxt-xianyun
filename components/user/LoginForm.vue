757cf70b1ad0baa422509f322cc30e0a0e5fc807
WickWen <mrwickweb@163.com>
2020-03-05 04:05:25 +0800

    发送前表单验证

--- components/user/LoginForm.vue
+++ components/user/LoginForm.vue
@@ -4,9 +4,11 @@
         ref="form"
         :rules="rules" 
         class="form">
-        <!-- :model  绑定数据模型  :rules 校验规则  prop需校验的字段名 -->
+        <!-- :model  绑定数据模型  :rules 校验规则 -->
+        <!-- ref 被用来给元素或子组件注册引用信息。如果在普通的 DOM 元素上使用，引用指向的就是 DOM 元素 -->
 
         <el-form-item class="form-item" prop='username'>
+         <!-- prop需校验的字段名 -->
             <el-input 
             placeholder="用户名/手机"
             v-model="form.username">
@@ -64,7 +66,24 @@ export default {
     methods: {
         // 提交登录
         handleLoginSubmit(){
-           console.log(this.form)
+            // 发送前保底验证一次,使用饿了么封装的方法
+            // this.$refs.form.validate((isValid, invalidField)=>{
+            // 1.回调的用法 第一个参数 是否校验成功的布尔值 第二个参数 是未通过校验的字段名 
+            //     if (isValid) {
+            //        console.log(this.form)
+            //     }
+            // });
+
+            // 2.promise 用法, 若不传入回调函数，则会返回一个 promise
+            this.$refs.form.validate().then(res=>{
+                if (res) {
+                    console.log(this.form)
+                }
+            }).catch(err=>{
+                console.log('校验失败');
+                console.log(err);
+            })
+
         }
     }
 }

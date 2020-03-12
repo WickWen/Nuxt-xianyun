8f0ae73d103027983edc2a745f24f0e9f81c5615
WickWen <mrwickweb@163.com>
2020-03-12 15:49:10 +0800

    创建机票列表页并引入航班信息组件

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,9 +1,44 @@
 <template>
-  <div>机票页</div>
+    <section class="contianer">
+        <el-row  type="flex" justify="space-between">
+
+            <!-- 顶部过滤列表 -->
+            <div class="flights-content">
+                <!-- 过滤条件 -->
+                <div>
+                    过滤条件
+                </div>
+                
+                <!-- 航班头部布局 -->
+                <div>
+                    <FlightsHeader></FlightsHeader>
+                </div>
+                
+                
+                <!-- 航班信息 -->
+                <div>
+                    <FlightsItem></FlightsItem>
+                </div>
+            </div>
+
+            <!-- 侧边栏 -->
+            <div class="aside">
+                <!-- 侧边栏组件 -->
+                侧边栏
+            </div>
+        </el-row>
+    </section>
 </template>
 
 <script>
+import FlightsHeader from '@/components/air/FlightsHeader'
+import FlightsItem from '@/components/air/FlightsItem'
 export default {
+  components:{
+    FlightsHeader,
+    FlightsItem
+  },
+
   mounted() {
     this.getFlightsList();
   },
@@ -17,11 +52,24 @@ export default {
       }).then(res => {
         console.log(res.data);
       });
-      
+
     }
   }
 };
 </script>
 
-<style>
+<style scoped lang="less">
+    .contianer{
+        width:1000px;
+        margin:20px auto;
+    }
+
+    .flights-content{
+        width:745px;
+        font-size:14px;
+    }
+
+    .aside{
+        width:240px;
+    } 
 </style>
